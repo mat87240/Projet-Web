b1d70c95c50cdf7c5372c79bb700e01296b06013
Meik0Yuu <dussot-a@3il.fr>
2025-02-23 15:01:33 +0100

    Add files via upload

---
+++ Projet-Web/Projet/footer.php
@@ -0,0 +1,13 @@
+<footer class="footer-container">
+    <div>
+        <div class="footer-logo">logo</div>
+        <nav class="footer-menu">
+            <a id="footer-mentions" href="mention-legal.php">Mentions légales</a>
+            <a id="footer-donnees" href="#">Données personnelles</a>
+            <a id="footer-cgu" href="#">Conditions Générales d'Utilisation</a>
+            <a id="footer-cookies" href="#">Gestion des cookies</a>
+            <a id="footer-plan" href="#">Plan du site</a>
+        </nav>
+        <div class="footer-icons"></div>
+    </div>
+</footer>
