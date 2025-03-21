7b044a7608cce8874729202ac1da71ae8b8feaa1
Meik0Yuu <dussot-a@3il.fr>
2025-03-21 13:53:24 +0100

    Delete Projet-Web-main/Projet-Web-main/Projet-Web/Projet directory

--- Projet-Web-main/Projet-Web-main/Projet-Web/Projet/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('BASE_PATH', $_SERVER['DOCUMENT_ROOT'] . "/website/Projet-Web/Projet/");
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'my_database');
-define('SECRET_KEY', '58MYLUnDm8HyEjALpMn21U37s1oCZdT1MFBb5RssOm5zmg3svsVDb3W5bt4K4VqLOveO9U4JvdHJlG2snAKcEQ0zS68U29dl6bAGABsM605hUmUh4Q573zoDjl8ukceG');
-?>
